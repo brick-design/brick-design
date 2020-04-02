e74846282ee7d15dfd429ce11f3941e0142ea06d
anye931123 <penghuishi@jieyuechina.com>
2020-04-02 15:51:04 +0800

    去掉代码分割，解决windows环境编译错误问题

--- .umirc.ts
+++ .umirc.ts
@@ -5,30 +5,6 @@ const config:IConfig={
   chainWebpack(config, { webpack }) {
     // 设置 alias
     config.resolve.alias.set('@', path.resolve(__dirname, 'src'));
-    config.optimization.splitChunks({
-      chunks: 'async',
-      minSize: 30000,
-      maxSize: 0,
-      minChunks: 1,
-      maxAsyncRequests: 6,
-      maxInitialRequests: 4,
-      automaticNameDelimiter: '~',
-      name: true,
-      cacheGroups: {
-        vendors: {
-          name: 'vendors',
-          chunks: 'all',
-          test: /[\\/]node_modules[\\/](react|react-dom|lodash|dva|prop-types|rc-animate|re-resizable|sortablejs|highlight.js)[\\/]/,
-          priority: -10,
-        },
-        antdesigns: {
-          name: 'antdesigns',
-          chunks: 'all',
-          test: /[\\/]node_modules[\\/](@ant-design|antd)[\\/]/,
-          priority: -11,
-        }
-      },
-    });
   },
   publicPath:'./',
   plugins: [
@@ -42,7 +18,6 @@ const config:IConfig={
         dva: true,
         antd: true,
         locale: true,
-        chunks: ['vendors','antdesigns', 'umi']
       },
     ],
 
